92431b8e54f3f5b1123975c73f4bcbf7ab586921
Leejaelim <149447215+Leejaelim@users.noreply.github.com>
2023-11-21 13:31:14 +0900

    Delete JL_TEST directory

--- JL_TEST/in/file_create.h
+++
@@ -1,9 +0,0 @@
-#ifndef __FILE_CREATE_H__
-#define __FILE_CREATE_H__
-
-#define ERROR               (-1)
-
-int file_open(char *filename, int option, unsigned int permission);
-void file_close(int file_descriptor);
-
-#endif
